7fc89ce33ed4cd933414d097da402304c7d90bf6
Joey Boerwinkel <joey@iconica.nl>
2023-01-24 17:46:13 +0100

    Add helloworld effect card

---
+++ lib/src/cards/effects/hello_world.dart
@@ -0,0 +1,11 @@
+import 'package:new_card_game/src/cards/move_types.dart';
+import 'package:new_card_game/src/effect.dart';
+
+const helloWorld = Effect(
+  cost: 20,
+  name: 'Hello World',
+  archetype: 'Cynet',
+  description:
+      'Revive one Cynet creature from the afterlife. Then, pay the resource cost',
+  moveType: [MoveTypes.ingnition],
+);
